cac37ea3a1fd50362ba7d341745ec02ea63c9a1d
Steven Knier <steven.knier@ecolab.com>
2019-05-05 12:58:37 -0500

    Deleted miscellaneous old files

--- saveverbs.py
+++
@@ -1,8 +0,0 @@
-#to save more verbs, edit the object in this file, run the file, and the file
-#verbs.p will be updated
-
-import pickle
-
-verbs = { "eat": {"eat"}, "take": {"take", "grab"}}
-
-pickle.dump( verbs, open( "verblist.p", "wb" ) )
