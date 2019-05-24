da0922354f1588c6a06fcc01cc0f400b356f2ab5
zurbrugc <zurbrugc@oregonstate.edu>
2019-05-24 10:11:31 -0700

    Reserving some items in the portrait gallery

--- Content/portraitGallery/portraitGallery.py
+++ Content/portraitGallery/portraitGallery.py
@@ -6,9 +6,12 @@ class PortraitGallery:
 	visible = False
 	aliases = []
 	descriptions = {'shortDesc': "You're in the portrait gallery again. ", 
-					'longDesc': "The door leads you to what looks like a... portrait gallery? "}
+				'longDesc': "The door leads you to an art gallery. One wall is covered in portraits of former residents of the mansion. They look down disapprovingly at you. The opposite wall contains mostly paintings of naked mythological figures in various seductive poses, but there's a landscape and a still life that look interesting. ",
+				'criticFragmentA': " There's a male art critic looking at one of the portraits.",
+				'criticFragmentB': " There's a female art critic looking at the large landscape.",
+				'criticFragmentBoth': " There are two critics (one male, one female) looking at different paintings."}
 	doors = {'north': 'barGalleryDoor', 'west': 'foyerGalleryDoor', 'south': 'galleryBilliardRoomDoor'}
-	items = []
+	items = ['still life', 'landscape', 'portraits', 'male critic', 'female critic', 'cabinet key']
 	properties = {'initialized': False}
 
 			
