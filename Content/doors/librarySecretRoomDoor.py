aafdc8c926605ea2cfe2454b5e28f39e0a834fbb
Deatona <46653615+Deatona-OSU@users.noreply.github.com>
2019-05-31 23:24:22 -0700

    Secret Room Door now locked
    Need bone key to open secret door

--- Content/doors/librarySecretRoomDoor.py
+++ Content/doors/librarySecretRoomDoor.py
@@ -2,14 +2,33 @@ import eng
 
 class LibrarySecretRoomDoor:
 	name = 'librarySecretRoomDoor'
-	visible = True 
+	visible = False 
 	aliases = ['door']
 	roomConnections = {'up': 'Library', 'down': 'Secret Room'}
-	descriptions = {'desc': "Upon further inspection... there's nothing special about this door. It looks like any other door you've ever seen."}
-	properties = {'locked': False}
-	
+	descriptions = {'desc': "It is an old iron door. Looks very sturdy.",
+				'lockedDoor': "The door is locked. ",
+				'unlockFail': "Lock didn't turn",
+				'unlockSuccess': "The old lock creeks and turns. The door opens to a stone stairwell leading downward. "}
+	properties = {'locked': True}
+
+	def open(self, otherThing):
+		if self.properties['locked']:
+			if otherThing is None:
+				key = eng.getItemByName('bone key')
+				if eng.inInventory(key):
+					return self._unlockDoor()
+				else:
+					return self.descriptions['unlockFail']
+			if otherThing.name == 'bone key':
+				return self._unlockDoor()
+			else:
+				return self.descriptions['unlockFail']
+		else:
+			return self.descriptions['alreadyOpen']	
 	
 	def go(self):
+		if self.properties['locked']:
+			return self.descriptions['lockedDoor']
 		library = eng.getRoomByName('Library')
 		secretRoom = eng.getRoomByName('Secret Room')
 		currRoom = eng.getCurrentRoom()
@@ -22,6 +41,11 @@ class LibrarySecretRoomDoor:
 	def look(self):
 		return self.descriptions['desc']
 
+	def _unlockDoor(self):
+		self.properties['locked'] = False
+		secretRoom = eng.getRoomByName('Secret Room')
+		secretRoom.visible = True
+		return self.descriptions['unlockSuccess']
 
 librarySecretRoomDoor = LibrarySecretRoomDoor()
 eng.setupDoor(librarySecretRoomDoor)
