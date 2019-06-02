81697777db7fda471914c9d2ad4ba1dab0af1449
Deatona <46653615+Deatona-OSU@users.noreply.github.com>
2019-06-01 23:57:59 -0700

    Updated CEO

--- Content/ballroom/charlesWinston4.py
+++ Content/ballroom/charlesWinston4.py
@@ -4,12 +4,12 @@ class CharlesWinston4:
 	name = 'CharlesWinston4'
 	#type = 'Item'
 	visible = True 
-	aliases = ['Charles', 'Winston', 'Charles Winston', 'Charles Winston IV', 'Master Winston', 'Sir Charles', 'Mr. Winston', 'CEO']
+	aliases = ['Charles', 'Winston', 'Charles Winston', 'Charles WinstonIV', 'Master Winston', 'Sir Charles', 'Mr. Winston', 'CEO']
 	descriptions = {'desc': "The CEO is male about 6'2 , looks about mid-fifies, graying hair on the sides, standing in the midst of three other people. He looks very important in the world of old money. It might be best to avoid him or raise suspicion.",
 					'intro': "Hello, nice to meet you I am Charles Winston IV. I don't recognize you.",
 					'converse':"YOU: I am a friend of, um um, your butler. CHARLES: My butler would never invite a friend to a Winston party. He knows his position.",
 					'shock':"Get your hands off me.",
-					'suspiciousTalk': "I don't know who you are, but you don't belong here. Wallace escort this man to the door.",
+					'suspiciousTalk': "I don't know who you are, but you don't belong here. Wallace escort this man to the door.\n...YOU HAVE RESTARTED IN MASTER BEDROOM. YOU WERE CAUGHT!! -10 points ",
 					'sharks': "Yes, I still have those leopard sharks in the basement. I can show you after the party. It will be about time to feed them anyways by then. You know it all started when Uncle Nathaniel brought their descendants back from his adventures. " }
 	properties = {'suspicious':False, 'conversation':False}
 	
@@ -20,6 +20,10 @@ class CharlesWinston4:
 
 	def talk(self):
 		if self.properties['suspicious']:
+			masterBedroom = eng.getRoomByName('Master Bedroom')	
+			score = eng.getScore()
+			eng.setScore(score - 10)
+			eng.goToRoom(masterBedroom) # Caught start over at Master Bedroom
 			return self.descriptions['suspiciousTalk']
 		elif self.properties['conversation']:
 			self.properties['suspicious'] = True
