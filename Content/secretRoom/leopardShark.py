792bb0fef5070d747f3ced8fe8f853a235fa6a95
zurbrugc <zurbrugc@oregonstate.edu>
2019-06-04 17:32:40 -0700

    Update leopardShark.py

--- Content/secretRoom/leopardShark.py
+++ Content/secretRoom/leopardShark.py
@@ -8,7 +8,8 @@ class Sharks:
 	descriptions = {'desc': "Five leopard sharks swimming around. They look like they are searching for dinner. ",
 					'docileDesc': "Five leopard sharks swimming around. After they ate the tune they seem much more docile. ",
 					'crazy': "Are you nuts! Those are hungry sharks. They aren't likely to eat humans, but you prize your limbs don't you? ",
-					'swim':"You get one finger on one of them, but it swims away fast. "}
+					'swim': "You get one finger on one of them, but it swims away quickly. ",
+					'takeSharks': "Nothing draws attention more than somebody carrying a flailing shark though a posh party."}
 	properties = {'hungry': True}
 	
 	
@@ -33,6 +34,9 @@ class Sharks:
 
 	def kill(self):
 		return self.touch()
+
+	def take(self):
+		return self.descriptions['takeSharks']
 
 sharks = Sharks()
 eng.setupItem(sharks)
