350967d652a1bcf07e74c1651c63029cdb0624d4
zurbrugc <zurbrugc@oregonstate.edu>
2019-06-04 17:32:32 -0700

    Update mouse.py

--- Content/secondBedroom/mouse.py
+++ Content/secondBedroom/mouse.py
@@ -14,7 +14,7 @@ class Mouse:
 					'dropNoHold': "You have to pick him up first.",
 					'dropYes': "You release the mouse into the room and hide behind the door. The mouse runs towards the maid and touches her leg. The maid wakes up. She looks around for a moment, sees the mouse, and then starts screaming. Moments later the butler enters the room.\n\nThe butler puts his arm around her and begins whispering some rather sappy things into her ear. They won't notice you, but you'd better slip out before you start to feel nauseated.",
 					'dropNo': "You reach for the mouse, but pause. There's a better place to let the little guy go.",
-					'dropButlerDead': "You release the mouse into the room. He runs under the bed and disappears. Oh well.",
+					'dropButlerDead': "You release the mouse into the room. He runs into a dark corner and disappears. Oh well.",
 					'nokill': "--No animals were harmed in the making of this game.--"}
 	properties = {}
 	
@@ -47,6 +47,7 @@ class Mouse:
 		if eng.inInventory(self) == False:
 			return self.descriptions['dropNoHold']
 		elif butler.properties['dead'] == True:
+			eng.removeFromInventory(self)
 			return self.descriptions['dropButlerDead']
 		else:
 			currRoom = eng.getCurrentRoom()
